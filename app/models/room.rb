27cc26ef501208911615e9c2dd028ec86f7f9e98
mikkee888 <watcharapongruttum@gmail.com>
2026-02-03 11:58:33 +0700

    room.rb 555

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,12 +1,3 @@
-# app/models/chat_room.rb
-class ChatRoom < ApplicationRecord
-  has_many :room_members, dependent: :destroy
-  has_many :delegates, through: :room_members
-  has_many :chat_messages, dependent: :destroy
-  
-  # กำหนด enum ให้ถูกต้อง
-  enum room_kind: { direct: 0, group: 1, event: 2 }, _prefix: true
-
-  
-  validates :title, presence: true
+# app/models/room.rb
+class Room < ApplicationRecord
 end
