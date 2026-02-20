2e4bf29749851df344265b6c8765f699718313b6
mikkee888 <watcharapongruttum@gmail.com>
2026-02-20 14:25:04 +0700

    Brevo sent mail test 44444 - 2026-02-20 14:25

--- app/jobs/reset_password_job.rb
+++ app/jobs/reset_password_job.rb
@@ -5,8 +5,21 @@ class ResetPasswordJob < ApplicationJob
     delegate = Delegate.find_by(id: delegate_id)
     return unless delegate
 
-    PasswordMailer.reset_password(delegate)
+    reset_url = "#{ENV['FRONTEND_URL']}/reset-password?token=#{delegate.reset_password_token}"
 
+    html = ApplicationController.render(
+      template: "password_mailer/reset_password",
+      assigns: {
+        reset_url: reset_url,
+        token: delegate.reset_password_token
+      }
+    )
+
+    BrevoMailService.send_email(
+      to: delegate.email,
+      subject: "Reset Your Password",
+      html: html
+    )
   rescue => e
     Rails.logger.error "[ResetPasswordJob] Failed: #{e.message}"
   end
