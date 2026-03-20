11035f0e936fc5210b51d75852fe2eeb9beb1a70
mikkee888 <watcharapongruttum@gmail.com>
2026-03-20 10:23:26 +0700

    feat: include activity slots in times_today - 2026-03-20 10:23

--- app/models/table.rb
+++ app/models/table.rb
@@ -413,17 +413,32 @@ class Table < ApplicationRecord
     table_number.to_s.strip
   end
 
+
+  
+
   def self.times_today_for(conf_date)
     return [] unless conf_date
 
-    Schedule
+    meeting_times = Schedule
       .where(conference_date_id: conf_date.id)
       .where("booker_id IS NOT NULL")
       .order(:start_at)
       .pluck(:start_at)
       .map { |t| t.in_time_zone(TIMEZONE).iso8601 }
-      .uniq
+
+    activity_times = ConferenceSchedule
+      .where(conference_date_id: conf_date.id)
+      .order(:start_at)
+      .pluck(:start_at)
+      .map { |t| t.in_time_zone(TIMEZONE).iso8601 }
+
+    (meeting_times + activity_times).uniq.sort
   end
+
+
+
+
+
 
   def self.conference_days_for(conference)
     return [] unless conference
